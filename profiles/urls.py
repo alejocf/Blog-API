3a12f8eab81c86655c05d1b7b34ac183d30e4fce
alejo <alejandrocf2835@gmail.com>
2025-09-09 15:01:40 -0500

    Profile Detail API View creation

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from profiles.views import ProfileEditView
+from profiles.views import ProfileDetailtAPIView, ProfileEditView
 
 
 urlpatterns = [
   path('edit-profile/', ProfileEditView.as_view(), name=''),
-  # path("profiles/", ProfileListAPIView.as_view(), name="")
+  path('my-profile/', ProfileDetailtAPIView.as_view(), name='')
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView
+from rest_framework.generics import RetrieveUpdateAPIView, RetrieveAPIView
 from rest_framework import permissions
 from profiles.models import Profile
 from profiles.serializers import ProfileSerializer
@@ -11,7 +11,9 @@ class ProfileEditView(RetrieveUpdateAPIView):
     profile, created = Profile.objects.get_or_create(user=self.request.user) # tuple
     return profile
 
-# class ProfileListAPIView(ListAPIView):
-#   serializer_class = ProfileSerializer
-#   model = Profile
-#   queryset = model.objects.all()
+class ProfileDetailtAPIView(RetrieveAPIView):
+  serializer_class = ProfileSerializer
+  permission_classes = [permissions.IsAuthenticated]
+
+  def get_object(self):
+    return self.request.user.profile
